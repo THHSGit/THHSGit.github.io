c63c7886181e81804fba1d66c2d90d0d8b494055
THHSGit <31997626+THHSGit@users.noreply.github.com>
2017-09-15 22:05:44 -0400

    Update THHS.Git.py

--- THHS.Git.py
+++ THHS.Git.py
@@ -1,1 +1,9 @@
-print('Hi')
+# quiz for project
+
+
+q1 = raw_input('What is the last line ooath state?')
+print ('a)  And thus, in all these ways, we will strive to transmit this city not only not less, but greater, better, and more beautiful than it was transmitted to us.')
+print ('b)  We will never bring disgrace to our city by any act of dishonesty or cowardice nor ever desert our suffering comrades in the ranks.')
+print ('c)
+
+
