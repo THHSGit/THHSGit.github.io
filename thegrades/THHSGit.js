9fc8f47341d1a13365f31bad611f7ba5509cecf9
THHSGit <31997626+THHSGit@users.noreply.github.com>
2017-11-10 14:02:11 -0500

    Create THHSGit.js

---
+++ thegrades/THHSGit.js
@@ -0,0 +1,10 @@
+function openNavr() {
+    document.getElementById("mySidenavr").style.width = "250px";
+}
+function openNavl() {
+    document.getElementById("mySidenavl").style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenavr").style.width = "0";
+    document.getElementById("mySidenavl").style.width = "0";
+} 
