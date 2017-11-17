9da9d5f9bc43a135f9bc487d3137b4e16f1aabf6
THHSGit <31997626+THHSGit@users.noreply.github.com>
2017-11-17 13:58:52 -0500

    Update THHSGit.js

--- thegrades/THHSGit.js
+++ thegrades/THHSGit.js
@@ -1,13 +1,10 @@
 function openNavr() {
     document.getElementById("mySidenavr").style.width = "250px";
-    document.body.style.backgroundColor = "rgba(255,255,255,0.4)";
 }
 function openNavl() {
     document.getElementById("mySidenavl").style.width = "250px";
-    document.body.style.backgroundColor = "rgba(255,255,255,0.4)";
 }
 function closeNav() {
     document.getElementById("mySidenavr").style.width = "0";
     document.getElementById("mySidenavl").style.width = "0";
-    document.body.style.backgroundColor = "rgba(0,0,0,1)";
 } 
