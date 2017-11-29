d6980b80f6b4f79720711918a8a08df856638136
THHSGit <31997626+THHSGit@users.noreply.github.com>
2017-11-29 14:06:51 -0500

    Update THHSGit.js

--- thegrades/THHSGit.js
+++ thegrades/THHSGit.js
@@ -1,11 +1,14 @@
 function openNavr() {
     document.getElementById("mySidenavr").style.width = "250px";
-    
+    document.getElementById("mySidenavr").style.zIndex = "1";
 }
 function openNavl() {
     document.getElementById("mySidenavl").style.width = "250px";
+    document.getElementById("mySidenavl").style.zIndex = "1";
 }
 function closeNav() {
     document.getElementById("mySidenavr").style.width = "0";
+    document.getElementById("mySidenavr").style.zIndex = "-1";
     document.getElementById("mySidenavl").style.width = "0";
+    document.getElementById("mySidenavl").style.zIndex = "-1";
 } 
