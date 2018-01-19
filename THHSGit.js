87e8c12b8d3f90e69f6966f12324796f7ba61f15
THHSGit <31997626+THHSGit@users.noreply.github.com>
2018-01-18 20:54:22 -0500

    Update THHSGit.js

--- THHSGit.js
+++ THHSGit.js
@@ -1,1 +1,34 @@
-//empty
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
+export default class MenuExampleBasic extends Component {
+  state = {}
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  render() {
+    const { activeItem } = this.state
+    return (
+      <Menu>
+        <Menu.Item
+          name='Home'
+          active={activeItem === 'Home'}
+          onClick={this.handleItemClick}
+        >
+          Home
+        </Menu.Item>
+        <Menu.Item
+          name='N/A'
+          active={activeItem === 'N/A'}
+          onClick={this.handleItemClick}
+        >
+          N/A
+        </Menu.Item>
+        <Menu.Item
+          name='N/A'
+          active={activeItem === 'N/A'}
+          onClick={this.handleItemClick}
+        >
+          N/A
+        </Menu.Item>
+      </Menu>
+    )
+  }
+}
