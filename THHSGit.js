7955a4e472e80a6c7d3b8d9312d51efb214141c6
THHSGit <31997626+THHSGit@users.noreply.github.com>
2018-01-29 22:10:42 -0500

    Update THHSGit.js

--- THHSGit.js
+++ THHSGit.js
@@ -1,20 +1,57 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+var slideIndexMovies = 1;
+showDivsMovies(slideIndexMovies);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusDivsMovies(n) {
+  showDivsMovies(slideIndexMovies += n);
 }
 
-function showDivs(n) {
+function showDivsMovies(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
+  var x = document.getElementsByClassName("mySlidesMovies");
+  if (n > x.length) {slideIndexMovies = 1}    
+  if (n < 1) {slideIndexMovies = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";  
+  x[slideIndexMovies-1].style.display = "block";  
 }
+
+var slideIndexAction = 1;
+showDivsAction(slideIndexAction);
+
+function plusDivsAction(n) {
+  showDivsAction(slideIndexAction += n);
+}
+
+function showDivsAction(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlidesAction");
+  if (n > x.length) {slideIndexAction = 1}    
+  if (n < 1) {slideIndexAction = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndexAction-1].style.display = "block";  
+}
+
+var slideIndexRomance = 1;
+showDivsRomance(slideIndexRomance);
+
+function plusDivsRomance(n) {
+  showDivsRomance(slideIndexRomance += n);
+}
+
+function showDivsRomance(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlidesRomance");
+  if (n > x.length) {slideIndexRomance = 1}    
+  if (n < 1) {slideIndexRomance = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndexRomance-1].style.display = "block";  
+}
+
 function openNavr() {
     document.getElementById("mySidenavr").style.width = "350px";
     document.getElementById("mySidenavr").style.zIndex = "1";
