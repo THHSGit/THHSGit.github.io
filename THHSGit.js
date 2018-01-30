bd5aea8ba3d534553693d582f919dc85478ee492
THHSGit <31997626+THHSGit@users.noreply.github.com>
2018-01-29 22:00:47 -0500

    Update THHSGit.js

--- THHSGit.js
+++ THHSGit.js
@@ -1,3 +1,20 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
 function openNavr() {
     document.getElementById("mySidenavr").style.width = "350px";
     document.getElementById("mySidenavr").style.zIndex = "1";
