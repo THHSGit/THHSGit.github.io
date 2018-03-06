6f1567810212b005c28a942f318c6c987c2377ac
THHSGit <31997626+THHSGit@users.noreply.github.com>
2018-03-06 14:25:53 -0500

    Update index.js

--- MarkFelt/index.js
+++ MarkFelt/index.js
@@ -6,14 +6,14 @@ function removeLoader() {
   document.getElementById("loader").style.display = "none";
 }
 function retract() {
-  if (i = 1) {
+  if (retract_status = 1) {
     document.getElementById("menu").style.display = "none";
     document.getElementById("reretractbutton").style.display = "block";
-    i = 0;
-  } else if (i = 0) {
+    retract_status = 0;
+  } else if (retract_status = 0) {
     document.getElementById("menu").style.display = "block";
     document.getElementById("reretractbutton").style.display = "none";
-    i = 1;
+    retract_status = 1;
   }
 }
 run();
