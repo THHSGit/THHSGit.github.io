f399e5689ddff3e3809629c268750f8f223c9bc9
THHSGit <31997626+THHSGit@users.noreply.github.com>
2018-03-06 13:41:39 -0500

    Update index.js

--- MarkFelt/index.js
+++ MarkFelt/index.js
@@ -1,7 +1,20 @@
+var retract_status = 1;
 function run() {    
   setTimeout(removeLoader, 2000);    
 }
 function removeLoader() {
   document.getElementById("loader").style.display = "none";
 }
+function retract() {
+  if (i = 1) {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("reretractbutton").style.display = "block";
+    i = 0;
+  } else if (i = 0) {
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("reretractbutton").style.display = "none";
+    i = 1
+  }
+}
 run();
+
