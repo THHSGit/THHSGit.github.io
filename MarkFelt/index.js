5c66774bff9b5a94c6110e9b2779e587f036badb
THHSGit <31997626+THHSGit@users.noreply.github.com>
2018-03-06 18:52:45 -0500

    Update index.js

--- MarkFelt/index.js
+++ MarkFelt/index.js
@@ -13,14 +13,14 @@ function removeLoader() {
 function retract() {
   if (retract_status === 1) {
     document.getElementById("menu").style.transition = "opacity 1s ease-in-out";
-    document.getElementById("menu").style.display = "none";
-    document.getElementById("reretractbutton").style.display = "block";
+    //document.getElementById("menu").style.display = "none";
+    //document.getElementById("reretractbutton").style.display = "block";
     document.getElementById("reretractbutton").style.transition = "opacity 1s ease-out-in";
     retract_status = 0;
   } else if (retract_status === 0) {
     document.getElementById("reretractbutton").style.transition = "opacity 1s ease-in-out";
-    document.getElementById("reretractbutton").style.display = "none";
-    document.getElementById("menu").style.display = "block";
+    //document.getElementById("reretractbutton").style.display = "none";
+    //document.getElementById("menu").style.display = "block";
     document.getElementById("menu").style.transition = "opacity 1s ease-out-in";
     retract_status = 1;
   }
