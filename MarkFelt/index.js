b50c3b2caf5992f63170f1a7f76934e3fbd920d0
THHSGit <31997626+THHSGit@users.noreply.github.com>
2018-03-06 18:47:23 -0500

    Update index.js

--- MarkFelt/index.js
+++ MarkFelt/index.js
@@ -1,22 +1,26 @@
 var retract_status = 1;
 function run() {    
-  setTimeout(removeLoader, 2000);    
+  setTimeout(removeLoaderMobile, 1500);    
+  setTimeout(removeLoader, 1500); 
+}
+function removeLoaderMobile() {
+  document.getElementById("loader-mobile-series").style.display = "none";
 }
 function removeLoader() {
   document.getElementById("loader").style.display = "none";
 }
 function retract() {
   if (retract_status === 1) {
-    document.getElementById("menu").style.transition = "opacity .25s ease-in-out";
+    document.getElementById("menu").style.transition = "opacity 1s ease-in-out";
     document.getElementById("menu").style.display = "none";
     document.getElementById("reretractbutton").style.display = "block";
-    document.getElementById("reretractbutton").style.transition = "opacity .25s ease-out-in";
+    document.getElementById("reretractbutton").style.transition = "opacity 1s ease-out-in";
     retract_status = 0;
   } else if (retract_status === 0) {
-    document.getElementById("reretractbutton").style.transition = "opacity .25s ease-in-out";
+    document.getElementById("reretractbutton").style.transition = "opacity 1s ease-in-out";
     document.getElementById("reretractbutton").style.display = "none";
     document.getElementById("menu").style.display = "block";
-    document.getElementById("menu").style.transition = "opacity .25s ease-out-in";
+    document.getElementById("menu").style.transition = "opacity 1s ease-out-in";
     retract_status = 1;
   }
 }
