f1eab975b10c688892cd42f55d9bd3ca2ddbe828
THHSGit <31997626+THHSGit@users.noreply.github.com>
2018-03-06 18:49:44 -0500

    Update index.js

--- MarkFelt/index.js
+++ MarkFelt/index.js
@@ -1,10 +1,12 @@
 var retract_status = 1;
+document.getElementById("loader").style.display = "none";
 function run() {    
   setTimeout(removeLoaderMobile, 1500);    
-  setTimeout(removeLoader, 1500); 
+  setTimeout(removeLoader, 3000); 
 }
 function removeLoaderMobile() {
   document.getElementById("loader-mobile-series").style.display = "none";
+  document.getElementById("loader").style.display = "block";
 }
 function removeLoader() {
   document.getElementById("loader").style.display = "none";
