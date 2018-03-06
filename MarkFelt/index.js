9d824e39ae9f8bd8f3a46f13ca2b315d4f9a8f8c
THHSGit <31997626+THHSGit@users.noreply.github.com>
2018-03-06 13:12:30 -0500

    Update index.js

--- MarkFelt/index.js
+++ MarkFelt/index.js
@@ -1,29 +1,3 @@
-import React from 'react'
-import { Dropdown, Menu } from 'semantic-ui-react'
-
-const DropdownExamplePointingTwo = () => (
-  <Menu vertical>
-    <Menu.Item>
-      Home
-    </Menu.Item>
-    <Menu.Item>
-      Story
-    </Menu.Item>
-    <Menu.Item>
-      Trivia Game
-    </Menu.Item>
-    <Dropdown text='About' pointing='left' className='link item'>
-      <Dropdown.Menu>
-        <Dropdown.Item>Editors</Dropdown.Item>
-        <Dropdown.Item>Bibliography</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  </Menu>
-)
-
-export default DropdownExamplePointingTwo
-
-
 function run() {    
   setTimeout(removeLoader, 2000);    
 }
