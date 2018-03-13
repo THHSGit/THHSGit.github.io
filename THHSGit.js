abcbdd2c482c5fbfd0ee9f45c1b2eee449bd30e8
THHSGit <31997626+THHSGit@users.noreply.github.com>
2018-03-12 21:57:42 -0400

    Update THHSGit.js

--- THHSGit.js
+++ THHSGit.js
@@ -1,114 +1,54 @@
+var retract_status = 1;
+
 function run() {
-    setTimeout(removeLoader, 2000);
+    setTimeout(removeLoaderMobile, 1500);
+    setTimeout(removeLoader, 3000);
 }
+
+function removeLoaderMobile() {
+    document.getElementById("loader-mobile-series").style.display = "none";
+    document.getElementById("loader").style.display = "block";
+}
+
 function removeLoader() {
     document.getElementById("loader").style.display = "none";
     plusDivsMovies(1);
+    plusDivsMovies(-1);
 }
+
+function retract() {
+    if (retract_status === 1) {
+        document.getElementById("menu").style.display = "none";
+        document.getElementById("reretractbutton").style.display = "block";
+        retract_status = 0;
+    } else if (retract_status === 0) {
+        document.getElementById("reretractbutton").style.display = "none";
+        document.getElementById("menu").style.display = "block";
+        retract_status = 1;
+    }
+}
+
 run();
 
 var slideIndexMovies = 1;
 
 showDivsMovies(slideIndexMovies);
 
-
-
 function plusDivsMovies(n) {
-
     showDivsMovies(slideIndexMovies += n);
-
 }
-
-
 
 function showDivsMovies(n) {
-
     var i;
-
     var x = document.getElementsByClassName("mySlidesMovies");
-
-    if (n > x.length) {slideIndexMovies = 1}
-
-    if (n < 1) {slideIndexMovies = x.length}
-
-    for (i = 0; i < x.length; i++) {
-
-        x[i].style.display = "none";
-
+    if (n > x.length) {
+        slideIndexMovies = 1
     }
-
-    x[slideIndexMovies-1].style.display = "block";
-
-}
-
-
-
-var slideIndexAction = 1;
-
-showDivsAction(slideIndexAction);
-
-
-
-function plusDivsAction(n) {
-
-    showDivsAction(slideIndexAction += n);
-
-}
-
-
-
-function showDivsAction(n) {
-
-    var i;
-
-    var x = document.getElementsByClassName("mySlidesAction");
-
-    if (n > x.length) {slideIndexAction = 1}
-
-    if (n < 1) {slideIndexAction = x.length}
-
-    for (i = 0; i < x.length; i++) {
-
-        x[i].style.display = "none";
-
+    if (n < 1) {
+        slideIndexMovies = x.length
     }
-
-    x[slideIndexAction-1].style.display = "block";
-
-}
-
-
-
-var slideIndexRomance = 1;
-
-showDivsRomance(slideIndexRomance);
-
-
-
-function plusDivsRomance(n) {
-
-    showDivsRomance(slideIndexRomance += n);
-
-}
-
-
-
-function showDivsRomance(n) {
-
-    var i;
-
-    var x = document.getElementsByClassName("mySlidesRomance");
-
-    if (n > x.length) {slideIndexRomance = 1}
-
-    if (n < 1) {slideIndexRomance = x.length}
-
     for (i = 0; i < x.length; i++) {
-
         x[i].style.display = "none";
-
     }
-
-    x[slideIndexRomance-1].style.display = "block";
-
+    x[slideIndexMovies - 1].style.display = "block";
 }
